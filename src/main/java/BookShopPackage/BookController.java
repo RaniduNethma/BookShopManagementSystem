5cb2e07004e84676d64b8bd20bc07a6088ccc41e
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-07-13 23:49:13 +0530

    Creatae GetById function
    create backend function for get data by id

--- src/main/java/BookShopPackage/BookController.java
+++ src/main/java/BookShopPackage/BookController.java
@@ -3,6 +3,8 @@ package BookShopPackage;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookController {
 
@@ -37,5 +39,39 @@ public class BookController {
 			e.printStackTrace();
 		}
 		return isSucess;
+	}
+	
+	//View Data (GetById)
+	public static List<BookModel> getById (String Id){
+		
+		int convertedID = Integer.parseInt(Id);
+		
+		ArrayList <BookModel> book = new ArrayList<>();
+		
+		try {
+			//Database Connection Call
+			con = DBConnection.getConnection();
+			stmt = con.createStatement();
+			
+			//SQL Query
+			String sql = "SELECT * FROM book WHERE id '"+convertedID+"'";
+			rs = stmt.executeUpdate(sql);
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String price = rs.getString(3);
+				String catagory = rs.getString(4);
+				String quantity = rs.getString(5);
+				String description = rs.getString(6);
+				
+				BookModel bk = new BookModel(id, name, price, catagory, quantity, description);
+				book.add(bk);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return book;
 	}
 }
